26088cd1b386f0afbe20d27f0a49075a8aed4d6b
IsaacGGP <20206364@uatx.mx>
2026-07-21 17:31:18 -0600

    chore: initialize Spring Boot project with hexagonal architecture

---
+++ backend/techinventory-service/src/main/java/org/isaac/techinventoryservice/TechinventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package org.isaac.techinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechinventoryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TechinventoryServiceApplication.class, args);
+    }
+}
---
+++ backend/techinventory-service/src/test/java/org/isaac/techinventoryservice/TechinventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isaac.techinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechinventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
